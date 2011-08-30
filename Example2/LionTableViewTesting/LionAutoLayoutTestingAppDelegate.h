3bf3c360bc2297ff72ee35b52651972763c7db21
Tomaz Kragelj <tkragelj@gmail.com>
2011-08-30 21:28:44 +0200

    Added empty example 2 project.

---
+++ Example2/LionTableViewTesting/LionAutoLayoutTestingAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  LionAutoLayoutTestingAppDelegate.h
+//  LionTableViewTesting
+//
+//  Created by Tomaž Kragelj on 8/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+@interface LionAutoLayoutTestingAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (nonatomic, assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Example2/LionTableViewTesting/LionAutoLayoutTestingAppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  LionAutoLayoutTestingAppDelegate.m
+//  LionTableViewTesting
+//
+//  Created by Tomaž Kragelj on 8/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LionAutoLayoutTestingAppDelegate.h"
+
+@implementation LionAutoLayoutTestingAppDelegate
+
+@synthesize window = _window;
+
+#pragma mark - NSApplicationDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)note {
+	// Nothing to do here for this simple app...
+}
+
+@end
