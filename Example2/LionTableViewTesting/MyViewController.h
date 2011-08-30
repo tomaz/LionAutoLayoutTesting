0dc4feeb396d6eac78859dd08a569e5096f09f0f
Tomaz Kragelj <tkragelj@gmail.com>
2011-08-30 21:59:11 +0200

    Added view controller with text field.

---
+++ Example2/LionTableViewTesting/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  LionAutoLayoutTesting
+//
+//  Created by Tomaž Kragelj on 8/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyViewController : NSViewController
+
+@end
---
+++ Example2/LionTableViewTesting/MyViewController.m
@@ -0,0 +1,23 @@
+//
+//  MyViewController.m
+//  LionAutoLayoutTesting
+//
+//  Created by Tomaž Kragelj on 8/30/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@implementation MyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
