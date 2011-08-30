f0ae7d98898fbfb17a741ad83fc952070dc1c41b
Tomaz Kragelj <tkragelj@gmail.com>
2011-08-30 20:04:39 +0200

    Implemented layout constraints for name and details labels.

--- Example1/LionTableViewTesting/ItemCellView.m
+++ Example1/LionTableViewTesting/ItemCellView.m
@@ -23,12 +23,24 @@ static BOOL kLargeSizeRequested = YES;
 
 - (void)awakeFromNib {
 	NSView *iconView = self.imageView;
-	NSDictionary *views = NSDictionaryOfVariableBindings(iconView);
+	NSView *nameView = self.textField;
+	NSView *detailView = self.detailTextField;
+	NSDictionary *views = NSDictionaryOfVariableBindings(iconView, nameView, detailView);
 	[self removeConstraints:self.constraints];
 	
-	// Pin icon 2 pixels from each edge.
+	// Pin icon 2 pixels from each edge and make it's horizontal size fit vertical.
 	[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-2-[iconView]" options:0 metrics:nil views:views]];
 	[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-2-[iconView]-2-|" options:0 metrics:nil views:views]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:iconView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:iconView attribute:NSLayoutAttributeHeight multiplier:1.0f constant:0.0f]];
+	
+	// Pin name field to the top and tie it few pixels to the right of the icon, all the way to the right edge of the cell.
+	[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"[iconView]-4-[nameView]-2-|" options:0 metrics:nil views:views]];
+	[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-1-[nameView]" options:0 metrics:nil views:views]];
+	
+	// Pin detail field's few pixels from the bottom and tie it's left and width to name field.
+	[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[detailView]-5-|" options:0 metrics:nil views:views]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:detailView attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:nameView attribute:NSLayoutAttributeLeft multiplier:1.0f constant:0.0f]];
+	[self addConstraint:[NSLayoutConstraint constraintWithItem:detailView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nameView attribute:NSLayoutAttributeWidth multiplier:1.0f constant:0.0f]];
 }
 
 - (void)setObjectValue:(id)objectValue {
